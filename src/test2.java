e35b2df6066a734c5b19eec717d543d1e5cf7504
nallapati <nallapati@sunil>
2022-04-23 13:15:30 +0530

    check it i updated

--- src/test2.java
+++ src/test2.java
@@ -3,8 +3,11 @@ public class test2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("i am sunil");
+		System.out.println("i am sunil e sariki ela");
 		System.out.println("ksudbaduf");
+		int a=10;
+		int b=23;
+		System.out.println(a+b);
 
 	}
 
