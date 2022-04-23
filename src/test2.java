bafdbe41f9dc62328c25317a3f57de5436680e41
nallapati <nallapati@sunil>
2022-04-23 12:43:46 +0530

    commiting test to sunilcode

--- src/test2.java
+++ src/test2.java
@@ -1,9 +1,10 @@
 
-public class demo1 {
+public class test2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello venky i modified code plz check");
+		System.out.println("i am sunil");
+		System.out.println("ksudbaduf");
 
 	}
 
